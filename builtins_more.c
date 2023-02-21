36bba8e0abf7780ab03172fdca5f61ffae12c201
tamarajoyce <tamarajoyce03@gmail.com>
2023-02-21 21:52:21 +0300

    builtins_more.c

--- builtins_more.c
+++ builtins_more.c
@@ -134,3 +134,26 @@ int builtin_help(data_of_program *data)
 	return (0);
 }
 
+/**
+ * builtin_alias - add, remove or show aliases
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_alias(data_of_program *data)
+{
+	int i = 0;
+
+	/* if there are no arguments, print all environment */
+	if (data->tokens[1] == NULL)
+		return (print_alias(data, NULL));
+
+	while (data->tokens[++i])
+	{/* if there are arguments, set or print each env variable*/
+		if (count_characters(data->tokens[i], "="))
+			set_alias(data->tokens[i], data);
+		else
+			print_alias(data, data->tokens[i]);
+	}
+
+	return (0);
+}
